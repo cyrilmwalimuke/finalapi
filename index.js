cc752dff5a0b11ac46e6816a70a04fd88e3cf604
jamesbrowncyril <jamesbrowncyril@gmail.com>
2024-06-15 23:56:49 +0300

    new changes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
-app.get('/',()=>{
-    res.send('heelo world')
+app.get('/',(req,res)=>{
+    res.json({
+        sucess:false
+    })
 })
 app.listen(3000,()=>{
     console.log("App is running oo port 5000")
