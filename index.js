939a61ab83165d764822a3c554c8c13b69f8b975
jamesbrowncyril <jamesbrowncyril@gmail.com>
2024-06-16 00:16:58 +0300

    commits

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const app = express();
-app.get( '/',(request,response)=>{
-    response.json({
-        sucess:false,
-        number:10
-    })
-})
+
 
 app.listen(5000,()=>{
     console.log("App itrs running oo port 5000")
 });
+app.get('/',(req,res)=>{
+    res.json({message:"success is coming"})
+})
