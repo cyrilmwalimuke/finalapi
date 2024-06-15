162a813b0a3374b8fde9dafe36d24d8ae4d1ce80
jamesbrowncyril <jamesbrowncyril@gmail.com>
2024-06-16 01:04:16 +0300

    changedport

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express();
 
 
-app.listen(5000,()=>{
+app.listen(3000,()=>{
     console.log("App itrs running oo port 5000")
 });
-app.get('/',(req,res)=>{
-    res.json({message:"success is coming"})
-})
