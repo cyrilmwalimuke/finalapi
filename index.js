cdbed6f857d022adf40f6248ed64015f5d99acad
jamesbrowncyril <jamesbrowncyril@gmail.com>
2024-06-16 01:27:43 +0300

    good progress

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.listen(3000,()=>{
     console.log("App ceduueu itrs running oo port 5000")
 });
 app.get('/',(req,res)=>{
-    res.send('hello there world')
+    res.json({
+        success:true
+
+    })
 })
 
