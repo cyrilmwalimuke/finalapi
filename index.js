c197f1d8998f77bf3d8ac9dad6813876bcd0e9d4
jamesbrowncyril <jamesbrowncyril@gmail.com>
2024-06-16 00:12:54 +0300

    news

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express();
+app.get( '/',(request,response)=>{
+    response.json({
+        sucess:false,
+        number:10
+    })
+})
 
 app.listen(3000,()=>{
     console.log("App itrs running oo port 5000")
