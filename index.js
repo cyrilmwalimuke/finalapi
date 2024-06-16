a361c156fc68e7463d856d2e4f7e1e94c7e152d8
jamesbrowncyril <jamesbrowncyril@gmail.com>
2024-06-16 10:42:16 +0300

    made changes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
 const app = express();
+app.use(cors())
+
 
 
 app.listen(3000,()=>{
-    console.log("App ceduueu itrs running oo port 5000")
+    console.log("App is running on port 5000")
 });
 app.get('/',(req,res)=>{
     res.json({
